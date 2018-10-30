2c839ea092800c6cb91bdd6a09046ca8759965de
CanhGosuu <safetycanh@gmail.com>
2018-10-30 17:41:15 +0700

    Add the item in the UI

--- Budgety1.0/app.js
+++ Budgety1.0/app.js
@@ -51,7 +51,9 @@ var UIController = (function() {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list"
   };
   return {
     getInput: function() {
@@ -63,6 +65,25 @@ var UIController = (function() {
     },
     getDOMstrings: function() {
       return DOMstrings;
+    },
+    addListItem: function(obj, type) {
+      //Create HTML string with placeholder text
+      var html, newHTML, element;
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      //Replace the placeholder  text with some actual data
+      newHTML = html.replace("%id%", obj.id);
+      newHTML = newHTML.replace("%description%", obj.description);
+      newHTML = newHTML.replace("%value%", obj.value);
+      //Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHTML);
     }
   };
 })();
@@ -71,13 +92,13 @@ var controller = (function(budgetControl, UIControl) {
   var DOM = UIControl.getDOMstrings();
 
   var ctrlAddItem = function() {
-    var input,
-      newItem,
-      // 1. Get the field input data
-      input = UIControl.getInput();
+    var input, newItem;
+    // 1. Get the field input data
+    input = UIControl.getInput();
     // 2. Add item to the bubget
     newItem = budgetControl.addItem(input.type, input.description, input.value);
     // 3. Add the item in the UI
+    UIControl.addListItem(newItem, input.type);
   };
 
   var setupEventListeners = function() {
